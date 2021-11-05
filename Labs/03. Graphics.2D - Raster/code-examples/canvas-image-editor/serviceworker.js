908b14cf25c89b123162b1fb2239761062d98e3a
Liviu-Adrian Cotfas <lcotfas@gmail.com>
2021-11-05 19:48:16 +0200

    Create serviceworker.js

---
+++ Labs/03. Graphics.2D - Raster/code-examples/canvas-image-editor/serviceworker.js
@@ -0,0 +1,49 @@
+const cacheName = 'image-editor-1';
+const filesToCache = [
+  'index.html',
+  'main.js',
+  'image-editor.js',
+  'image-editor.css',
+];
+
+//1. Cache the required files
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+//2. Delete the old version of the cache
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
+  return self.clients.claim();
+});
+
+//3. Fetch files from the cache or from the internet
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+
+  e.respondWith(
+    caches.match(e.request)
+      .then(function(response) {
+        return response || fetch(e.request);
+        //https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+        //fetch - returns a promise that resolves to the Response to that request, whether it is successful or not
+      })
+  );
+});
