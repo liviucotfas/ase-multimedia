034cba973d5f43d4648c0570a034bb3fb09173ac
erik kovacs <kovaroberik@gmail.com>
2021-12-03 23:37:45 +0200

    refactored each entity into its own class

--- Labs/11. Games/code-examples/canvas-game/game.js
+++ Labs/11. Games/code-examples/canvas-game/game.js
@@ -1,66 +1,271 @@
-class Game {
-    #canvas = null;
-    #context = null;
-
-    // Brick settings
-    #brickColumnCount = 5;
-    #brickRowCount = 3;
-    #brickWidth = 0;
-    #brickHeight = 20;
-    #brickPadding = 10;
-    #brickOffsetTop = 30;
-    #brickOffsetLeft = 30;
-
-    // Paddle settings
-    #paddleHeight = 10;
-    #paddleWidth = 75;
-    #paddleX = null;
-
+class Ball {
     // Ball settings
-    #ballRadius = 10;
+    #radius = 10;
+
+    // we want to add a small value to x and y after every frame has been drawn to make it appear that the ball is moving
+    // dx and dy can be tuned for gameplay quality and difficulty
     #dxDefault = 2;
     #dyDefault = -2;
     #dx = null;
     #dy = null;
+
     #x = 0;
     #y = 0;
 
-    // Keyboard - store information on whether the left or right control button is pressed
-    #rightKeyPressed = false;
-    #leftKeyPressed = false;
+    constructor() {
+        this.centre();
+    }
 
-    // Game data
-    #score = 0;
-    #lives = 3;
+    centre() {
+        this.#x = Game.canvas.width / 2;
+        this.#y = Game.canvas.height - 30;
+        this.#dx = this.#dxDefault;
+        this.#dy = this.#dyDefault;
+    }
+
+    draw() {
+        Game.context.beginPath();
+        Game.context.arc(this.#x, this.#y, this.#radius, 0, Math.PI * 2);
+        Game.context.fillStyle = '#FF0000';
+        Game.context.fill();
+        Game.context.closePath();
+    }
+
+    moveAndCollide(paddle, bricks) {
+        // Bouncing off the left and right
+        if (this.#x + this.#dx > Game.canvas.width - this.#radius || this.#x + this.#dx < this.#radius) {
+            this.#dx = -this.#dx;
+        }
+
+        // Bouncing off the top
+        if (this.#y + this.#dy < this.#radius) {
+            this.#dy = -this.#dy;
+        }
+        // Bouncing off the bottom
+        else if (this.#y + this.#dy > Game.canvas.height - this.#radius) {
+            if (this.#x > paddle.x && this.#x < paddle.x + paddle.width) {
+                this.#dy = -this.#dy;
+            } else {
+                // Subtract a life if player missed the ball
+                Game.subtractLife();
+            }
+        }
+
+        // Hitting a brick destroys it, earns a point, and bounces the ball
+        for (let brick of bricks) {
+            if (brick.isAlive()) {
+                if (this.#x > brick.x &&
+                    this.#x < brick.x + brick.width &&
+                    this.#y > brick.y &&
+                    this.#y < brick.y + brick.height
+                ) {
+                    this.#dy = -this.#dy;
+                    brick.destroy();
+                    Game.score();
+                }
+            }
+        }
+
+        // Update the location of the ball
+        this.#x += this.#dx;
+        this.#y += this.#dy;
+    }
+}
+
+class Paddle {
+    // Paddle settings
+    #width = 75;
+    #height = 10;
+    #x = null;
+
+    get width() { return this.#width; }
+    get height() { return this.#height; }
+    get x() { return this.#x; }
+
+    constructor() {
+        this.centre();
+    }
+
+    centre() { this.#x = (Game.canvas.width - this.#width) / 2; }
+
+    handleUserInput(input) {
+        // we can control the position of the paddle by checking for input and updating it each frame
+        const { type, e } = input;
+        let relativeX;
+
+        switch (type) {
+            case 'mouse':
+                // peg the movement of the paddle to the relative position 
+                // of the pointer within the game canvas
+                relativeX = e.clientX - Game.canvas.offsetLeft;
+                if (relativeX > 0 && relativeX < Game.canvas.width) {
+                    this.#x = relativeX - this.#width / 2;
+                }
+                break;
+            case 'keyboard':
+                // move the paddle right if the right control button is pressed
+                if (Game.rightKeyPressed && this.#x < Game.canvas.width - this.#width) {
+                    this.#x += 7;
+                }
+                // move the paddle left if the left control button is pressed
+                else if (Game.leftKeyPressed && this.#x > 0) {
+                    this.#x -= 7;
+                }
+                break;
+            case 'touch':
+                relativeX = input.e.touches[0].clientX;
+                if (relativeX > 0 && relativeX < Game.canvas.width) {
+                    this.#x = relativeX - paddleWidth / 2;
+                }
+                break;
+        }
+    }
+
+    draw() {
+        Game.context.beginPath();
+        Game.context.rect(this.#x, Game.canvas.height - this.#height, this.#width, this.#height);
+        Game.context.fillStyle = '#0095DD';
+        Game.context.fill();
+        Game.context.closePath();
+        // equivalent to `context.fillRect()`
+    }
+}
+
+class Brick {
+    // Brick settings
+    #width = 0;
+    #height = 0;
+    #x = 0;
+    #y = 0;
+    #alive = true;
+
+    set x(x) { this.#x = x; }
+    set y(y) { this.#y = y; }
+
+    get width() { return this.#width; }
+    get height() { return this.#height; }
+    get x() { return this.#x; }
+    get y() { return this.#y; }
+
+    constructor(x, y, width, height) {
+        this.#x = x;
+        this.#y = y;
+        this.#width = width;
+        this.#height = height;
+        this.#alive = true;
+    }
+
+    isAlive() { return this.#alive; }
+
+    destroy() { this.#alive = false; }
+
+    draw() {
+        Game.context.beginPath();
+        Game.context.rect(this.#x, this.#y, this.#width, this.#height);
+        Game.context.fillStyle = '#0095DD';
+        Game.context.fill();
+        Game.context.closePath();
+    }
+}
+
+class BrickMatrix {
+    // Brick collection class
+    #rowCount = 0;
+    #columnCount = 0;
+
+    #individualBrickWidth = 0;
+    #individualBrickHeight = 20;
+    #individualBrickPadding = 10;
+
+    #offsetTop = 30;
+    #offsetLeft = 30;
+
+    // keep bricks in a flat array because it's easier to iterate over them
     #bricks = [];
 
-    constructor(canvas) {
+    constructor(rowCount, columnCount) {
+        this.#rowCount = rowCount;
+        this.#columnCount = columnCount;
+
+        const brickTotalWidth = (Game.canvas.width - 2 * this.#offsetLeft) / this.#columnCount;
+        this.#individualBrickWidth = brickTotalWidth - this.#individualBrickPadding;
+
+        // Initialise bricks
+        for (let i = 0; i < this.size(); i++) {
+            this.#bricks.push(
+                new Brick(0, 0, this.#individualBrickWidth, this.#individualBrickHeight)
+            );
+        }
+    }
+
+    size() { return this.#rowCount * this.#columnCount; }
+
+    // Implement iterator so we can use `for (let brick of bricks) {}`
+    // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators
+    [Symbol.iterator]() {
+        let index = -1;
+        let data = this.#bricks;
+
+        return {
+            next: () => ({ value: data[++index], done: !(index in data) })
+        };
+    };
+
+    draw() {
+        // traverse as if it were a matrix in order to calculate positions
+        for (let r = 0; r < this.#rowCount; r++) {
+            for (let c = 0; c < this.#columnCount; c++) {
+                // get index for flat array
+                const i = r * this.#columnCount + c;
+                if (this.#bricks[i].isAlive()) {
+                    const brickX = (c * (this.#individualBrickWidth + this.#individualBrickPadding)) + this.#offsetLeft;
+                    const brickY = (r * (this.#individualBrickHeight + this.#individualBrickPadding)) + this.#offsetTop;
+                    this.#bricks[i].x = brickX;
+                    this.#bricks[i].y = brickY;
+                    this.#bricks[i].draw();
+                }
+            }
+        }
+    }
+}
+
+class Game {
+    // Elements we use to calculate positions and draw 
+    static #canvas = null;
+    static #context = null;
+
+    // Entities
+    static #ball = null;
+    static #paddle = null;
+    static #bricks = null;
+
+    // Keyboard - store information on whether the left or right control button is pressed
+    static #rightKeyPressed = false;
+    static #leftKeyPressed = false;
+
+    static get canvas() { return this.#canvas; }
+    static get context() { return this.#context; }
+    static get rightKeyPressed() { return this.#rightKeyPressed; }
+    static get leftKeyPressed() { return this.#leftKeyPressed; }
+
+    // Game state
+    static #score = 0;
+    static #lives = 3;
+
+    static init(canvas) {
         this.#canvas = canvas;
         this.#context = this.#canvas.getContext('2d');
 
         this.#resize();
 
-        // Ball settings
-        this.#x = this.#canvas.width / 2;
-        this.#y = this.#canvas.height - 30;
+        // Initialise ball
+        this.#ball = new Ball();
 
-        // we want to add a small value to x and y after every frame has been drawn to make it appear that the ball is moving
-        this.#dx = this.#dxDefault;
-        this.#dy = this.#dyDefault;
+        // Initialise paddle
+        this.#paddle = new Paddle();
 
-        // Paddle settings
-        this.#paddleX = (this.#canvas.width - this.#paddleWidth) / 2;
-
-        const brickTotalWidth = (this.#canvas.width - 2 * this.#brickOffsetLeft) / this.#brickColumnCount;
-        this.#brickWidth = brickTotalWidth - this.#brickPadding;
-
-        // Game data
-        for (let r = 0; r < this.#brickRowCount; r++) {
-            this.#bricks[r] = [];
-            for (let c = 0; c < this.#brickColumnCount; c++) {
-                this.#bricks[r][c] = { x: 0, y: 0, status: 1 };
-            }
-        }
+        // Initialise a 3 * 5 matrix of bricks
+        this.#bricks = new BrickMatrix(3, 5);
 
         // Attach events
         //
@@ -75,14 +280,7 @@ class Game {
         document.addEventListener('mousemove', e => this.#mouseMoveHandler(e), false);
     }
 
-    #touchMove(e) {
-        const relativeX = e.touches[0].clientX;
-        if (relativeX > 0 && relativeX < this.#canvas.width) {
-            paddleX = relativeX - paddleWidth / 2;
-        }
-    }
-
-    #resize() {
+    static #resize() {
         const width = this.#canvas.clientWidth;
         const height = this.#canvas.clientHeight;
         if (this.#canvas.width != width ||
@@ -92,7 +290,11 @@ class Game {
         }
     }
 
-    #keyDownHandler(e) {
+    static #touchMove(e) {
+        this.#paddle.handleUserInput({ type: 'touch', e: e });
+    }
+
+    static #keyDownHandler(e) {
         if (e.keyCode == 39) {
             this.#rightKeyPressed = true;
         }
@@ -101,7 +303,7 @@ class Game {
         }
     }
 
-    #keyUpHandler(e) {
+    static #keyUpHandler(e) {
         if (e.keyCode == 39) {
             this.#rightKeyPressed = false;
         }
@@ -110,141 +312,71 @@ class Game {
         }
     }
 
-    #mouseMoveHandler(e) {
-        const relativeX = e.clientX - this.#canvas.offsetLeft;
-        if (relativeX > 0 && relativeX < this.#canvas.width) {
-            this.#paddleX = relativeX - this.#paddleWidth / 2;
-        }
+    static #mouseMoveHandler(e) {
+        this.#paddle.handleUserInput({ type: 'mouse', e: e });
     }
 
-    #drawBall() {
-        this.#context.beginPath();
-        this.#context.arc(this.#x, this.#y, this.#ballRadius, 0, Math.PI * 2);
-        this.#context.fillStyle = '#FF0000';
-        this.#context.fill();
-        this.#context.closePath();
-    }
-
-    #drawPaddle() {
-        this.#context.beginPath();
-        this.#context.rect(this.#paddleX, this.#canvas.height - this.#paddleHeight, this.#paddleWidth, this.#paddleHeight);
-        this.#context.fillStyle = '#0095DD';
-        this.#context.fill();
-        this.#context.closePath();
-        // equivalent to app.context.fillRect
-    }
-
-    #drawBricks() {
-        for (let r = 0; r < this.#brickRowCount; r++) {
-            for (let c = 0; c < this.#brickColumnCount; c++) {
-                if (this.#bricks[r][c].status == 1) {
-                    const brickX = (c * (this.#brickWidth + this.#brickPadding)) + this.#brickOffsetLeft;
-                    const brickY = (r * (this.#brickHeight + this.#brickPadding)) + this.#brickOffsetTop;
-                    this.#bricks[r][c].x = brickX;
-                    this.#bricks[r][c].y = brickY;
-                    this.#context.beginPath();
-                    this.#context.rect(brickX, brickY, this.#brickWidth, this.#brickHeight);
-                    this.#context.fillStyle = '#0095DD';
-                    this.#context.fill();
-                    this.#context.closePath();
-                }
-            }
-        }
-    }
-
-    #drawScore() {
+    static #drawScore() {
         this.#context.font = '16px Arial';
         this.#context.fillStyle = '#000000';
         this.#context.fillText('Score: ' + this.#score, 8, 20);
     }
 
-    #drawLives() {
+    static #drawLives() {
         this.#context.font = '16px Arial';
         this.#context.fillStyle = '#000000';
         this.#context.fillText('Lives: ' + this.#lives, this.#canvas.width - 65, 20);
     }
 
-    draw() {
+    static #win() {
+        alert('YOU WIN, CONGRATS!');
+        document.location.reload();
+    }
+
+    static #lose() {
+        alert('GAME OVER');
+        document.location.reload();
+    }
+
+    static score() {
+        this.#score++;
+        if (this.#score == this.#bricks.size()) {
+            this.#win();
+        }
+    }
+
+    static subtractLife() {
+        this.#lives--;
+        this.#paddle.centre();
+        this.#ball.centre();
+        if (this.#lives < 1) {
+            this.#lose();
+        }
+    }
+
+    static draw() {
         this.#resize();
 
         // clear the canvas before each frame
         this.#context.clearRect(0, 0, this.#canvas.width, this.#canvas.height);
-        
+
         // draw the elements
-        this.#drawBricks();
-        this.#drawBall();
-        this.#drawPaddle();
+        this.#bricks.draw();
+        this.#ball.draw();
+        this.#paddle.draw();
+
         this.#drawScore();
         this.#drawLives();
-        
-        // detect collisions
-        this.#collisionDetection();
 
-        // Bouncing off the left and right
-        if (this.#x + this.#dx > this.#canvas.width - this.#ballRadius || this.#x + this.#dx < this.#ballRadius) {
-            this.#dx = -this.#dx;
-        }
+        // move the ball and check for collisions with the paddle and the bricks
+        this.#ball.moveAndCollide(this.#paddle, this.#bricks);
 
-        // Bouncing off the top
-        if (this.#y + this.#dy < this.#ballRadius) {
-            this.#dy = -this.#dy;
+        // handle paddle input
+        if (this.#leftKeyPressed || this.#rightKeyPressed) {
+            this.#paddle.handleUserInput({ type: 'keyboard' });
         }
-        // Bouncing off the bottom
-        else if (this.#y + this.#dy > this.#canvas.height - this.#ballRadius) {
-
-            if (this.#x > this.#paddleX && this.#x < this.#paddleX + this.#paddleWidth) {
-                this.#dy = -this.#dy;
-            }
-            else {
-                this.#lives--;
-                if (!this.#lives) {
-                    alert('GAME OVER');
-                    document.location.reload();
-                }
-                else {
-                    this.#x = this.#canvas.width / 2;
-                    this.#y = this.#canvas.height - 30;
-                    this.#dx = this.#dxDefault;
-                    this.#dy = this.#dyDefault;
-                    //center the paddle
-                    this.#paddleX = (this.#canvas.width - this.#paddleWidth) / 2;
-                }
-            }
-        }
-
-        // move the paddle right if the right control button is pressed
-        if (this.#rightKeyPressed && this.#paddleX < this.#canvas.width - this.#paddleWidth) {
-            this.#paddleX += 7;
-        }
-        // move the paddle left if the left control button is pressed
-        else if (this.#leftKeyPressed && this.#paddleX > 0) {
-            this.#paddleX -= 7;
-        }
-
-        // update the location of the ball
-        this.#x += this.#dx;
-        this.#y += this.#dy;
 
         // schedule the next iteration for the next frame
         requestAnimationFrame(() => this.draw());
-    }
-
-    #collisionDetection() {
-        for (let r = 0; r < this.#brickRowCount; r++) {
-            for (let c = 0; c < this.#brickColumnCount; c++) {
-                const b = this.#bricks[r][c];
-                if (b.status == 1) {
-                    if (this.#x > b.x && this.#x < b.x + this.#brickWidth && this.#y > b.y && this.#y < b.y + this.#brickHeight) {
-                        this.#dy = -this.#dy;
-                        b.status = 0;
-                        this.#score++;
-                        if (this.#score == this.#brickColumnCount * this.#brickRowCount) {
-                            alert('YOU WIN, CONGRATS!');
-                            document.location.reload();
-                        }
-                    }
-                }
-            }
-        }
     }
 }
