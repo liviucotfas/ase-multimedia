868e6f1a8634d8b937531f7a520cae80c5341e46
Liviu-Adrian Cotfas <lcotfas@gmail.com>
2022-11-22 17:11:13 +0200

    Changed the visibility of the canvas property to private

--- Labs/03. Graphics.2D - Raster/code-examples/canvas-bar-chart/canvas-bar-chart-library.js
+++ Labs/03. Graphics.2D - Raster/code-examples/canvas-bar-chart/canvas-bar-chart-library.js
@@ -2,25 +2,29 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
 class BarChart{
     /**
+     * The canvas on which the chart will be displayed
+     */
+    #canvas;
+    /**
      * 
      * @param {HTMLCanvasElement} canvas - The canvas used for drawing the histogram
      */
     constructor(canvas) {
-        this.canvas = canvas;
+        this.#canvas = canvas;
     }
     /**
      * 
      * @param {Array<number>} values - The values that will be displayed in the chart
      */
     draw(values){
-        const context = this.canvas.getContext('2d');
+        const context = this.#canvas.getContext('2d');
     
         // Save the current context of the applciation. We will restore it later.
         context.save();
     
         // Draw the chart background
         context.fillStyle = '#DEDEDE';
-        context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        context.fillRect(0, 0, this.#canvas.width, this.#canvas.height);
     
         // Set the color for the bars
         context.fillStyle = 'red';
@@ -29,15 +33,15 @@ class BarChart{
         
         //...spread operator
         const maxValue = Math.max(...values);
-        const f = this.canvas.height / maxValue;
+        const f = this.#canvas.height / maxValue;
 
-        const barWidth = this.canvas.width / values.length;
+        const barWidth = this.#canvas.width / values.length;
     
         for (let i = 0; i < values.length; i++) {
                
             const barHeight = values[i] * f * 0.9;
             const barX = i * barWidth + barWidth / 4;
-            const barY = this.canvas.height - barHeight;
+            const barY = this.#canvas.height - barHeight;
     
             context.fillRect(barX, barY, barWidth / 2, barHeight);
             context.strokeRect(barX, barY, barWidth / 2, barHeight);
